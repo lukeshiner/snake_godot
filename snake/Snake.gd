9de4dc3e261c579154d648b6b0b190e47a7804db
Luke Shiner <luke@lukeshiner.com>
2020-08-07 00:54:17 +0100

    Initial Commit

---
+++ snake/Snake.gd
@@ -0,0 +1,89 @@
+extends Node2D
+
+export(int) var start_length = 3
+
+const SnakeSprite = preload("res://snake/snake_sprite.tscn")
+const sprite_size = 16
+
+const directions = [Vector2.UP, Vector2.LEFT, Vector2.RIGHT, Vector2.DOWN]
+var direction = Vector2.RIGHT
+var next_direction = direction
+var apple_eaten = false
+
+onready var head = $SnakeSprite
+onready var sections = [head]
+
+func _ready():
+	randomize()
+	head.set_sprite(Vector2.ZERO, Vector2.LEFT)
+	var start_position = Vector2(global_position.x, global_position.y)
+	global_position = Vector2.ZERO
+	head.global_position = start_position
+	for i in range(start_length, 0, -1):
+		add_section(
+			Vector2(head.global_position.x - sprite_size*i, head.global_position.y),
+			head.global_position,
+			Vector2(head.global_position.x - sprite_size*(i+1), head.global_position.y)
+		)
+	set_tail()
+
+func add_section(location, previous, next):
+	var new_section = SnakeSprite.instance()
+	self.add_child(new_section)
+	new_section.global_position = location
+	var directions = new_section.get_directions(previous, next)
+	new_section.set_sprite(directions[0], directions[1])
+	sections.insert(1, new_section)
+
+func set_tail():
+	var last_section = sections[-1]
+	var directions = last_section.get_directions(sections[-2].global_position, null)
+	last_section.set_sprite(directions[0], directions[1])
+
+func _input(event):
+	if event.is_action_released("up"):
+		next_direction = Vector2.UP
+	elif event.is_action_released("left"):
+		next_direction = Vector2.LEFT
+	elif event.is_action_released("down"):
+		next_direction = Vector2.DOWN
+	elif event.is_action_released("right"):
+		next_direction = Vector2.RIGHT
+
+func update_direction():
+	if next_direction != direction * -1:
+		direction = next_direction
+
+func remove_end():
+	var remove_section = sections.pop_back()
+	remove_section.queue_free()
+	set_tail()
+
+func update_length():
+	if not apple_eaten:
+		remove_end()
+	else:
+		apple_eaten = false
+
+func _on_Timer_timeout():
+	update_direction()
+	var old_location = head.global_position
+	head.global_position += direction * sprite_size
+	var head_directions = head.get_directions(null, old_location)
+	head.set_sprite(head_directions[0], head_directions[1])
+	add_section(
+		old_location,
+		head.global_position,
+		sections[1].global_position
+	)
+	update_length()
+
+
+func _on_Area2D_area_entered(area):
+	var collided_layer = area.collision_layer
+	if collided_layer == 8:
+		apple_eaten = true
+		print("eat")
+	else:
+		print(collided_layer)
+		print(area.get_parent())
---
+++ snake/snake_sprite.gd
@@ -0,0 +1,87 @@
+extends Area2D
+
+onready var sprite = $Sprite
+
+enum {
+	HEAD_UP,
+	HEAD_RIGHT,
+	HEAD_DOWN,
+	HEAD_LEFT,
+	BODY_UP,
+	BODY_DOWN,
+	BODY_LEFT,
+	BODY_RIGHT,
+	TAIL_RIGHT,
+	TAIL_UP,
+	TAIL_DOWN,
+	TAIL_LEFT,
+	BEND_LEFT_UP,
+	BEND_RIGHT_UP,
+	BEND_UP_LEFT,
+	BEND_UP_RIGHT,
+	BEND_DOWN_LEFT,
+	BEND_DOWN_RIGHT,
+	BEND_RIGHT_DOWN,
+	BEND_LEFT_DOWN,
+}
+
+#0	HEAD_UP,
+#1	HEAD_RIGHT,
+#2	HEAD_DOWN,
+#3	HEAD_LEFT,
+#4	BODY_UP,
+#5	BODY_DOWN,
+#6	BODY_LEFT,
+#7	BODY_RIGHT,
+#8	TAIL_RIGHT,
+#9	TAIL_UP,
+#10	TAIL_DOWN,
+#11	TAIL_LEFT,
+#12	BEND_LEFT_UP,
+#13	BEND_RIGHT_UP,
+#14	BEND_UP_LEFT,
+#15	BEND_UP_RIGHT,
+#16	BEND_DOWN_LEFT,
+#17	BEND_DOWN_RIGHT,
+#18	BEND_RIGHT_DOWN,
+#19	BEND_LEFT_DOWN,
+
+
+var directions = {
+	[Vector2.ZERO, Vector2.DOWN]: HEAD_UP,
+	[Vector2.ZERO, Vector2.RIGHT]: HEAD_LEFT,
+	[Vector2.ZERO, Vector2.UP]: HEAD_DOWN,
+	[Vector2.ZERO, Vector2.LEFT]: HEAD_RIGHT,
+	[Vector2.UP, Vector2.DOWN]: BODY_DOWN,
+	[Vector2.UP, Vector2.RIGHT]: BEND_LEFT_UP,
+	[Vector2.UP, Vector2.LEFT]: BEND_DOWN_LEFT,
+	[Vector2.DOWN, Vector2.UP]: BODY_UP,
+	[Vector2.DOWN, Vector2.RIGHT]: BEND_RIGHT_DOWN,
+	[Vector2.DOWN, Vector2.LEFT]: BEND_UP_RIGHT,
+	[Vector2.LEFT, Vector2.UP]: BEND_RIGHT_UP,
+	[Vector2.LEFT, Vector2.RIGHT]: BODY_RIGHT,
+	[Vector2.LEFT, Vector2.DOWN]: BEND_LEFT_DOWN,
+	[Vector2.RIGHT, Vector2.UP]: BEND_DOWN_RIGHT,
+	[Vector2.RIGHT, Vector2.LEFT]: BODY_LEFT,
+	[Vector2.RIGHT, Vector2.DOWN]: BEND_UP_LEFT,
+	[Vector2.RIGHT, Vector2.ZERO]: TAIL_RIGHT,
+	[Vector2.UP, Vector2.ZERO]: TAIL_UP,
+	[Vector2.DOWN, Vector2.ZERO]: TAIL_DOWN,
+	[Vector2.LEFT, Vector2.ZERO]: TAIL_LEFT,
+}
+
+func get_directions(headward, tailward):
+	var direction_to_previous
+	var direction_to_next
+	if headward == null:
+		direction_to_previous = Vector2.ZERO
+	else:
+		direction_to_previous = global_position.direction_to(headward).normalized()
+	if tailward == null:
+		direction_to_next = Vector2.ZERO
+	else:
+		direction_to_next = global_position.direction_to(tailward).normalized()
+	return [direction_to_previous, direction_to_next]
+
+func set_sprite(direction_to_previous, direction_to_next):
+	sprite.frame = directions[[direction_to_previous, direction_to_next]]
